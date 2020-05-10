966e3d8915b7729aaebfd10a6cdeadfa677c795c
Muhammad Aziz Al-assad <muhazizal.26@gmail.com>
2020-05-10 11:06:39 +0700

    vuex before restructuring file

---
+++ vue-vuex/src/components/AnotherCounter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="asyncIncrement({by: 10, duration: 1000})">Increment</button>
+    <button class="btn btn-primary" @click="asyncDecrement({by: 10, duration: 1000})">Decrement</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  methods: {
+    ...mapActions(["asyncIncrement", "asyncDecrement"])
+  }
+};
+</script>
---
+++ vue-vuex/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="increment(10)">Increment</button>
+    <button class="btn btn-primary" @click="decrement(10)">Decrement</button>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+
+export default {
+  methods: {
+    ...mapMutations(["increment", "decrement"])
+  }
+};
+</script>
