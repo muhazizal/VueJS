97a8b51c0bad9d9b3e19806778ee4bf8b76d8eb3
muhazizal <muhazizal.26@gmail.com>
2020-06-06 19:34:27 +0700

    vue-axios done

--- vue-axios/src/components/dashboard/dashboard.vue
+++ vue-axios/src/components/dashboard/dashboard.vue
@@ -2,15 +2,51 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>your email: {{ email }}</p>
   </div>
 </template>
 
-<style scoped>
-  h1, p {
-    text-align: center;
-  }
+<script>
+import axios from "../../auth";
 
-  p {
-    color: red;
+export default {
+  data() {
+    return {
+      email: ""
+    };
+  },
+
+  created() {
+    axios
+      .get("/users.json")
+      .then(resposne => console.log(resposne))
+      .catch(error => console.log(error));
+
+    axios
+      .get("/users.json")
+      .then(response => {
+        const data = response.data;
+        const users = [];
+        for (let key in data) {
+          const user = data[key];
+          user.id = key;
+          users.push(user);
+        }
+        console.log(users);
+        this.email = users[0].email;
+      })
+      .catch(error => console.log("error"));
   }
+};
+</script>
+
+<style scoped>
+h1,
+p {
+  text-align: center;
+}
+
+p {
+  color: red;
+}
 </style>
--- vue-axios/src/main.js
+++ vue-axios/src/main.js
@@ -1,12 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+// import axios from 'axios';
 
-import router from './router'
-import store from './store'
+import router from './router';
+import store from './store';
+
+// axios.defaults.baseURL = 'https://vue-axios-484fe.firebaseio.com';
+// axios.defaults.headers.common['Authorization'] = 'Auth';
+// axios.defaults.headers.get['Accepts'] = 'application/json';
+
+// const reqInterceptor = axios.interceptors.request.use(config => {
+// 	console.log('Request Interceptors', config);
+// 	return config;
+// });
+
+// const resInterceptors = axios.interceptors.response.use(response => {
+// 	console.log('Response Interceptors', response);
+// 	return response;
+// });
 
 new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
-})
+	el: '#app',
+	router,
+	store,
+	render: h => h(App),
+});
