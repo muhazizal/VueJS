3611d0d84014119653b40bca79841b5f7530dada
muhazizal <muhazizal.26@gmail.com>
2021-10-10 11:00:17 +0700

    composition api 01

---
+++ composition-01/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="container">
+    <h2>{{ user.name }}</h2>
+    <p>{{ user.age }}</p>
+    <button @click="setAge">Change Age</button>
+  </section>
+</template>
+
+<script>
+// import { ref } from 'vue';
+import { reactive } from 'vue';
+export default {
+  setup() {
+    // const username = ref('Muhammad Aziz Al-assad');
+    // const age = ref(21);
+    const user = reactive({
+      name: 'Muhammad Aziz Al-assad',
+      age: 21
+    });
+
+    const setAge = () => {
+      user.age = 22;
+    };
+
+    return { user, setAge };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
---
+++ composition-assignment-01/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <h2>My Course Goal</h2>
+    <!-- Task 1: Output your main course goal with help of the composition API -->
+    <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+    <!-- <h3 v-if="isVisible">{{ courseGoal }}</h3> -->
+    <h3 v-if="goal.visibility">{{ goal.title }}</h3>
+    <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+    <button @click="setVisibleCourseGoal">Toggle Goal</button>
+    <!-- Task 3: Manage data in three ways -->
+    <!-- => Separate refs -->
+    <!-- => Ref Object -->
+    <!-- => Reactive Object -->
+    <!-- Task 4: Also solve the assignment with the Options API -->
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+// import { reactive } from 'vue';
+export default {
+  // data() {
+  //   return {
+  //     courseGoal: 'Learning Composition API',
+  //     isVisible: false
+  //   };
+  // },
+  // methods: {
+  //   setVisibleCourseGoal() {
+  //     this.isVisible = !this.isVisible;
+  //   }
+  // }
+  setup() {
+    // *Separate refs
+    // const courseGoal = ref('Learning Composition API');
+    // const isVisible = ref(false);
+
+    // const setVisibleCourseGoal = () => {
+    //   isVisible.value = !isVisible.value;
+    // };
+
+    // *Ref object
+    const goal = ref({
+      title: 'Learning Composition API',
+      visibility: false
+    });
+
+    const setVisibleCourseGoal = () => {
+      goal.value.visibility = !goal.value.visibility;
+    };
+
+    // *Reactive object
+    // const goal = reactive({
+    //   title: 'Learning Composition API',
+    //   visibility: false
+    // });
+
+    // const setVisibleCourseGoal = () => {
+    //   goal.visibility = !goal.visibility;
+    // };
+
+    return { goal, setVisibleCourseGoal };
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
