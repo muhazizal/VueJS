789539c17d673a3acdb3dce0bb6fab7524f0b59d
muhazizal <muhazizal.26@gmail.com>
2022-01-12 06:56:34 +0700

    reusability options api

---
+++ composition-reuse-01/src/components/AddUser.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <user-alert v-if="alertIsVisible" :title="title" @close="hideAlert">
+      <p>Do you want to continue with adding a user?</p>
+    </user-alert>
+    <section>
+      <h2>Add a User</h2>
+      <button @click="showAlert">Add User</button>
+    </section>
+  </div>
+</template>
+
+<script>
+import UserAlert from './UserAlert.vue';
+import alertMixin from '../mixins/alert';
+
+export default {
+  mixins: [alertMixin],
+  components: {
+    UserAlert,
+  },
+  data() {
+    return {
+      title: 'Add a User?',
+    };
+  },
+};
+</script>
